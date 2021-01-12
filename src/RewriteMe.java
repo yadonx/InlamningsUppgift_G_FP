960622cf4235bb790d055819b6971cdb76e63b33
yadonx <ej_yo@hotmail.com>
2021-01-12 11:53:52 +0100

    clean up

--- src/RewriteMe.java
+++ src/RewriteMe.java
@@ -1,8 +1,6 @@
-import java.util.Comparator;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
